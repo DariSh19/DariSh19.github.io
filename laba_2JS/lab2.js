af1f69ebaa95f5fdfb5cbf38519d2e3471187ae9
DariSH19 <sh.dasha999@yandex.ru>
2024-05-30 23:19:58 +0300

    Добавила лабы 2,3

--- laba_2JS/lab2.js
+++ laba_2JS/lab2.js
@@ -1,120 +1,75 @@
 /**
- * 1.Этот код возводит в степень n число x
- * @param {number} x основание
- * @param {number} n степень
- * @returns x^n
+ * 1. Возвращает x, возведенное в n-ную степень.
+ * @param {number} x Возводимое в степень число
+ * @param {number} n Степень, натуральное число
+ * @returns {number} Результат
  */
-let x = prompt("Введите х (число возводимое в степень)",1);
-let n = prompt("Введите n (степень)",1);
-alert(x + "^" + n + " = "+pow(x, n));
-function pow(x,n) {
-    if (n === 0) {
-      return 1;
+function pow(x, n){return x**n}
+//-------------------------------------------------------------------
+/**
+ * 2. Считаем сумму чисел до n включительно.
+ * @param {number} n Число до которого считаем сумму
+ * @returns {number} 
+ */
+function sumTo(n){
+    let s = 0
+    for (let i=0; i<=n; i++) s+=i
+    return s
+}
+//-------------------------------------------------------------------
+/**
+ * 3. Проверяет является ли год высокостным
+ * @param {number} year
+ * @returns {boolean} Вывод: Да/Нет
+ */
+function isLeapYear(year){return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)}
+//-------------------------------------------------------------------
+/**
+ * 4. Считает факториал числа n
+ * @param {number} n 
+ * @returns {bigint} Факториал числа n в формате BigInt
+ */
+function factorial(n){
+    if (n == 0) return BigInt(1)
+    if (n == 1) return BigInt(1)
+    if (n >= 2) return BigInt(n) * BigInt(factorial(n - 1));
+}
+//-------------------------------------------------------------------
+/**
+ * 5. Считает n-ное число Фибоначчи
+ * @param {number} n Порядковый номер числа 
+ * @returns {bigint}  n-е число Фибоначчи
+ */
+function fib(n){
+    if (!n) return BigInt(0)
+    let a = BigInt(1)
+    let b = BigInt(1)
+    for (let i = 3; i <= n; i++) {
+      let c = a + b
+      a = b
+      b = c
     }
-  
-    if (n < 0) {
-      x = 1 / x;
-      n = -n;
-    }
-  
-    let result = 1;
-    while (n > 0) {
-      result *= x;
-      n--;
-    }
-    return result;  
-  }
-//-------------------------------------------------------------------
-/**
- * 2.Вычисляет сумму всех целых чисел от 1 до n.
- * @param {number} n целое положительное число.
- * @returns {number} Сумма всех целых чисел от 1 до n
- */
-function sumTo(n) {
-  if (typeof n === 'number' && Number.isInteger(n) && n > 0) {
-      let sum = 0;
-      for (let i = 1; i <= n; i++) {
-          sum += i;
-      }
-      return sum;
-  } else {
-      throw new Error("n должно быть целым положительным числом.");
-  }
-}
-//-------------------------------------------------------------------
-/** 
- * 3.Проверяет, является ли год високосным
- * @param {number} year год, который нужно проверить.
- * @returns true, если год високосный, иначе false
-*/
-function isLeapYear(year) {
-  if (typeof year === 'number' && Number.isInteger(year)) {
-      return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
-  } else {
-      throw new Error("year должно быть целым числом.");
-  }
+    return b
 }
 //-------------------------------------------------------------------
 /**
- * 4.Этот код возвращает факториал числа n!, используя рекурсивный вызов.
- * @param {number} n число
- * @returns факториал числа n!
- */
-function factorial(n) {
-  if (n) 
-    return (n * factorial(n-1));
-  else 
-    return 1; 
-}
-//-------------------------------------------------------------------
-/**
- * 5.Этот код возвращает n-е число Фибоначчи.
- * Возвращаемое значение должно иметь тип BigInt.
- * @param {number} n число
- * @returns n-е число Фибоначчи
- */
-function fib(n) {
-  let a = 1n;
-  let b = 1n;
-  let c;
-  if (n==0) return 0;
-  if (n==1) return 1;
-  if (n==2) return 1;
-  for (let i = 3; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
-  }
-  if (b==1n)
-    b = 0;
-  return b; 
-}
-//-------------------------------------------------------------------
-/**
- * 6. принимает целочисленное значение x и возвращает анонимную функцию
+ * 6. Проверяет, больше ли y, чем x
+ * Если равны, возвращает null
  * @param {number} x
- * @return функцию сравнения
+ * @param {number} y
+ * @returns
  */
-function compare(x){
-	let result = function(y){
-    if (y > x) return true;
-    else if (y < x) return false;
-    else return null;
-  }
-  return result;
-}
+function compare(x){return function(y){return y==x ? null : y>x}}
 //-------------------------------------------------------------------
 /**
- * 7. вычисляет сумму всех переданных чисел
- * @param {} numbers - Числа для суммирования
- * @return сумма всех чисел
+ * 7. Считает сумму заданных чисел
+ * @param  {...number} args Набор чисел
+ * @returns {number} Сумма 
  */
-function sum(...numbers) {
-  let total = 0;
-  for (let number of numbers) {
-    total += number;
-  }
-  return total;
+function sum(...args){
+    let s = 0
+    for(let i of args) s+=i
+    return s
 }
 //-------------------------------------------------------------------
 /**
